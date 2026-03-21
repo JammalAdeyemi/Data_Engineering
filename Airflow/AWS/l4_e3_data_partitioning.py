8ede1592de13a3a0fedc8feebbddda42ad2711ef
Adeyemi Jammal <32106343+JammalAdeyemi@users.noreply.github.com>
2026-03-21 17:23:14 +0000

    update

--- Airflow/AWS/l4_e3_data_partitioning.py
+++ Airflow/AWS/l4_e3_data_partitioning.py
@@ -9,16 +9,29 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 import sql_statement
 
 @dag(
-    start_date=datetime(2026, 3, 21)
+    start_date=datetime(2018, 1, 1),
+    end_date=datetime(2018, 2, 1,),
+    schedule_interval='@monthly',
+    max_active_runs=1    
 )
-def data_lineage():
+def data_partitioning():
 
 
     @task()
-    def load_trip_data_to_redshift():
+    def load_trip_data_to_redshift(*args, **kwargs):
         metastoreBackend = MetastoreBackend()
         aws_connection=metastoreBackend.get_connection("aws_credentials")
         redshift_hook = PostgresHook("redshift")
+        # # #
+        # TODO: How do we get the execution_date from our context?
+        # execution_date=kwargs["<REPLACE>"]
+        execution_date = pendulum.now()
+        # # #
+
+        # # #
+        # TODO: modify the parameters when formatting sql_statements.COPY_ALL_TRIPS_SQL
+        # to include the year and month the pipeline executed
+        #
         sql_stmt = sql_statement.COPY_ALL_TRIPS_SQL.format(
             aws_connection.login,
             aws_connection.password,
@@ -46,19 +59,14 @@ def data_lineage():
         sql=sql_statement.CREATE_TRIPS_TABLE_SQL
     )
 
+
     create_stations_table = PostgresOperator(
         task_id="create_stations_table",
         postgres_conn_id="redshift",
         sql=sql_statement.CREATE_STATIONS_TABLE_SQL,
     )
 
-    calculate_traffic_task = PostgresOperator(
-        task_id='calculate_location_traffic',
-        postgres_conn_id="redshift",
-        sql=[sql_statement.LOCATION_TRAFFIC_SQL_DROP, sql_statement.LOCATION_TRAFFIC_SQL_CREATE],
-    )
-
-    create_trips_table >> load_trip_data_to_redshift_task >> calculate_traffic_task
+    create_trips_table >> load_trip_data_to_redshift_task
     create_stations_table >> load_station_data_to_redshift_task
 
-data_pipeline_schdules_dag = data_lineage()
+data_partitioning_dag = data_partitioning()
