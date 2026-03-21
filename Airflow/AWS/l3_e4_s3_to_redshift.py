2cf1cf8a8c4031b4758ed83e005261c15ba54515
Adeyemi Jammal <32106343+JammalAdeyemi@users.noreply.github.com>
2026-03-21 14:19:08 +0000

    push

--- Airflow/AWS/l3_e4_s3_to_redshift.py
+++ Airflow/AWS/l3_e4_s3_to_redshift.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import logging
-
+import sys
 from airflow.decorators import dag, task
 from airflow.secrets.metastore import MetastoreBackend
 from airflow.hooks.postgres_hook import PostgresHook
@@ -19,13 +19,13 @@ def load_data_to_redshift():
         aws_connection=metastoreBackend.get_connection("aws_credentials")
 # TODO: create the redshift_hook variable by calling PostgresHook()
         redshift_hook=PostgresHook("redshift")
-        redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(aws_connection.login, aws_connection.password))
+        redshift_hook.run(sql_statement.COPY_ALL_TRIPS_SQL.format(aws_connection.login, aws_connection.password))
 
 # TODO: create the create_table_task by calling PostgresOperator()
     create_table_task = PostgresOperator(
         task_id="create_table",
         postgres_conn_id="redshift",
-        sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+        sql=sql_statement.CREATE_TRIPS_TABLE_SQL
     )
 
 # TODO: create the location_traffic_task by calling PostgresOperator()
